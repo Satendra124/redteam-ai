7f50f39b2d47cf6b058ca76a80da24e4a109b9bc
Satendra Raj <satendra.raj1241@gmail.com>
2024-08-31 09:21:34 +0000

    run.sh changes

--- run.sh
+++ run.sh
@@ -1,4 +1,1 @@
-#!/bin/bash
-
-CHECKPOINT_DIR=/home/ubuntu/.llama/checkpoints/Meta-Llama3.1-8B-Instruct
-PYTHONPATH=$(git rev-parse --show-toplevel) torchrun main.py $CHECKPOINT_DIR
+nohup torchrun api.py &
